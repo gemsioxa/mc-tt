6afe0c487c0a42b9e979f2d4a2a17dce02d6a8ce
Backwards <deema.light@gmail.com>
2023-03-20 21:44:21 +0300

    refactor: add type to time options

--- src/Components/Editor/TextArea/TextArea.tsx
+++ src/Components/Editor/TextArea/TextArea.tsx
@@ -1,35 +1,51 @@
-import { useContext } from 'react'
-import {Box, Typography, TextareaAutosize, TextField} from '@mui/material'
-import { NotesContext } from '../../../context'
+import { useContext } from "react";
+import { Box, Typography, TextareaAutosize, TextField } from "@mui/material";
+import { NotesContext } from "../../../context";
 
 function TextArea() {
-  let options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
-  const {notes, setNewNotes, activeIndex, changeActiveIndex, addNewNote, removeNote, changeIsActive} = useContext(NotesContext)
-  const activeNote = notes[activeIndex]
-  const noteDate = new Date(notes[activeIndex].date)
+  let options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const {
+    notes,
+    setNewNotes,
+    activeIndex,
+    changeActiveIndex,
+    addNewNote,
+    removeNote,
+    changeIsActive,
+  } = useContext(NotesContext);
+  const activeNote = notes[activeIndex];
+  const noteDate = new Date(notes[activeIndex].date);
   return (
-    <Box sx={{
-        // background:'red',
-        width: '100%',
-        height: 'calc(100vh - 65px)',
-        maxHeight: 'calc(100vh - 65px)',
-        overflowY: 'auto',
-    }}>
-        <Typography textAlign='center' sx={{
-            width: '100%',
-            fontSize: '0.9rem',
-            fontWeight: '600',
-            color: '#9e9e9f',
-            margin: '10px 0',
-            fontFamily: 'Roboto'
-        }}>
-            {`${noteDate.toLocaleString('ru', options) + ' в ' + noteDate.toLocaleTimeString('ru').slice(0, -3)}`}
-        </Typography>
-        {/* <TextareaAutosize
+    <Box
+      sx={{
+        width: "100%",
+        height: "calc(100vh - 65px)",
+        maxHeight: "calc(100vh - 65px)",
+        overflowY: "auto",
+      }}
+    >
+      <Typography
+        textAlign="center"
+        sx={{
+          width: "100%",
+          fontSize: "0.9rem",
+          fontWeight: "600",
+          color: "#9e9e9f",
+          margin: "10px 0",
+          fontFamily: "Roboto",
+        }}
+      >
+        {`${
+          noteDate.toLocaleString("ru", options) +
+          " в " +
+          noteDate.toLocaleTimeString("ru").slice(0, -3)
+        }`}
+      </Typography>
+      {/* <TextareaAutosize
           style={{ 
             overflowY: 'scroll',
             height: 'calc(100vh - 120px)', 
@@ -44,59 +60,69 @@ function TextArea() {
             fontSize: '1rem'
           }}
         /> */}
-        <TextField
-          id={'textArea_edit'}
-          multiline
-          defaultValue={notes[activeIndex].title ? notes[activeIndex].title + '\n' + notes[activeIndex].text : notes[activeIndex].text}
-          variant="standard"
-          sx={{
-            width: '100%',
-            height: 'calc(100vh - 110px)',
-            '.css-1rb63tl-MuiInputBase-root-MuiInput-root': {
-                padding: '0 15px',
-                color: 'white',
-                '&:before': {
-                    border: 'none'
-                },
-                '&:after': {
-                    border: 'none'
-                },
-                '&:hover:not(.Mui-disabled, .Mui-error):before': {
-                    border: 'none'
-                }
+      <TextField
+        id={"textArea_edit"}
+        multiline
+        defaultValue={
+          notes[activeIndex].title
+            ? notes[activeIndex].title + "\n" + notes[activeIndex].text
+            : notes[activeIndex].text
+        }
+        variant="standard"
+        sx={{
+          width: "100%",
+          height: "calc(100vh - 110px)",
+          ".css-1rb63tl-MuiInputBase-root-MuiInput-root": {
+            padding: "0 15px",
+            color: "white",
+            "&:before": {
+              border: "none",
             },
-          }} onChange={e => {
-            notes &&          
-            setNewNotes(notes.map((note, i) => {
-              if (i !== activeIndex) return note
-              note.title = e.target.value.split('\n')[0]
-              note.text = e.target.value.split('\n').slice(1).join('\n')
-              note.date = new Date().toString()
-              
-              return note
-            }))
-            if (activeIndex != 0) {
-              setNewNotes(notes.sort((a, b) => {
-                const aSort = new Date(`${a.date}`)
-                const bSort = new Date(`${b.date}`)
-                if (aSort > bSort) return -1
-                else return 1
-              }))
-              changeActiveIndex(0)
-              changeIsActive(false)
-              changeIsActive(true)
-              // addNewNote({
-              //   'title': e.target.value.split('\n')[0],
-              //   'text': e.target.value.split('\n').slice(1).join('\n'),
-              //   'date': new Date().toString()
-              // })
-              // removeNote(activeIndex + 1)
-              // changeIsActive(true)
-            }
-            localStorage.setItem('notes', JSON.stringify(notes))
-          }}/>         
+            "&:after": {
+              border: "none",
+            },
+            "&:hover:not(.Mui-disabled, .Mui-error):before": {
+              border: "none",
+            },
+          },
+        }}
+        onChange={(e) => {
+          notes &&
+            setNewNotes(
+              notes.map((note, i) => {
+                if (i !== activeIndex) return note;
+                note.title = e.target.value.split("\n")[0];
+                note.text = e.target.value.split("\n").slice(1).join("\n");
+                note.date = new Date().toString();
+
+                return note;
+              })
+            );
+          if (activeIndex != 0) {
+            setNewNotes(
+              notes.sort((a, b) => {
+                const aSort = new Date(`${a.date}`);
+                const bSort = new Date(`${b.date}`);
+                if (aSort > bSort) return -1;
+                else return 1;
+              })
+            );
+            changeActiveIndex(0);
+            changeIsActive(false);
+            changeIsActive(true);
+            // addNewNote({
+            //   'title': e.target.value.split('\n')[0],
+            //   'text': e.target.value.split('\n').slice(1).join('\n'),
+            //   'date': new Date().toString()
+            // })
+            // removeNote(activeIndex + 1)
+            // changeIsActive(true)
+          }
+          localStorage.setItem("notes", JSON.stringify(notes));
+        }}
+      />
     </Box>
-  )
+  );
 }
 
-export default TextArea
+export default TextArea;
